3ed47dad7858e1f51205690323edb2cde593ceed
ammara-Ilyas <ismarailyas545@gmail.com>
2024-03-09 16:25:42 +0500

    next.js

---
+++ src/app/image/page.js
@@ -0,0 +1,39 @@
+"use client";
+import { useEffect, useState } from "react";
+import "./imageSearch.css";
+export default function imageSearch() {
+  const [query, setQuery] = useState();
+  const [imageArray, setImageArray] = useState([]);
+  useEffect(() => {
+    let accessKey = "VG4HY3iiinchcwaxgyEpZ7A7ogG81UOWZkdV0GomubM";
+    let url = `https://api.unsplash.com/search/photos?query=${query}&client_id=${accessKey}`;
+    const fetchData = async () => {
+      const response = await fetch(url);
+      let data = await response.json();
+      console.log(data);
+    };
+  });
+  return (
+    <div className="container">
+      <div className="box">
+        <h2>AI Image Generator</h2>
+        <div className="search-input">
+          <input type="text" className="input" />
+          <button className="btn">Search</button>
+        </div>
+        <div className="grid-container">
+          <div className="grid-item">
+            <img
+              src="https://images.unsplash.com/photo-1501908734255-16579c18c25f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlJTIwYmVhdXR5fGVufDB8fDB8fHww"
+              alt="nature Beauty"
+            />
+            <a href="#" className="download-btn">
+              <i className="fa-solid fa-download"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+      <button className="showMore">Show more</button>
+    </div>
+  );
+}
---
+++ src/app/random/page.js
@@ -0,0 +1,112 @@
+"use client";
+
+import { useState } from "react";
+import "./Random.css";
+export default function page() {
+  const [guess, setGuess] = useState("");
+  const [guessingArray, setGuessingArray] = useState([]);
+  const [attempts, setAttempts] = useState(10);
+  const [guessNo, setGuessNo] = useState(Math.floor(Math.random() * 100));
+  const [message, setMessage] = useState("");
+  const [gamePlay, setGamePlay] = useState(true);
+  const [number, setNumber] = useState();
+
+  const startGame = () => {
+    setGamePlay(true);
+    setAttempts(10);
+    setGuessingArray([]);
+  };
+  const checkingGuessNo = () => {
+    if (guess == guessNo) {
+      setGamePlay(false);
+      return setMessage("Congratulation,You win! ");
+    } else if (guess > guessNo) {
+      return setMessage("Your number is Too greater than guessing Number");
+    } else if (guess < guessNo) {
+      return setMessage("Your number is Too smaller than guessing Number");
+    }
+  };
+
+  const checkAttempt = () => {
+    if (attempts == 1) {
+      setMessage("Oh no, you lost the game!  ");
+      setGamePlay(false);
+    }
+  };
+  const checkNum = () => {
+    if (guess === "") {
+      alert("Write the Number");
+      setNumber(false);
+      return setMessage("");
+    } else {
+      if (guess > 100) {
+        setNumber(false);
+        return setMessage("The number should be smaller than 100");
+      } else if (guess < 0) {
+        setNumber(false);
+        return setMessage("The number should be greater than zero");
+      } else {
+        setNumber(true);
+      }
+    }
+    setGuess("");
+  };
+
+  const submitGuessNo = (e) => {
+    e.preventDefault();
+    checkNum();
+    if (number) {
+      setGuessingArray((preArr) => [guess, ...preArr]);
+      console.log(guessingArray);
+      setAttempts(attempts - 1);
+    }
+    console.log(guess);
+    setGuess("");
+    checkingGuessNo();
+    checkAttempt();
+    console.log(guessNo);
+  };
+  return (
+    <div>
+      <div className="container">
+        <div className="heading">
+          <h1>Number Guessing Game</h1>
+          <p>Guess the number between 1 and 100</p>
+          <p>You have 10 attempts to guess the number</p>
+        </div>
+        <form action="" className="form" onSubmit={submitGuessNo}>
+          <h1>Guess the Number</h1>
+          <input
+            type="text"
+            className="inp"
+            value={guess}
+            onChange={gamePlay ? (e) => setGuess(e.target.value) : undefined}
+          />
+          <input type="submit" className="btn" />
+        </form>
+        <div className="result">
+          <p>
+            Previous guesses
+            {`[ ${guessingArray} ]`}
+            {/* [
+            {guessingArray.map((guess, index) => (
+              <span key={index}> {guess} </span>
+            ))}
+            ] */}
+          </p>
+          <p>Guesses remaining {attempts}</p>
+          <div className="newGame">
+            <h2>{message}</h2>
+            {gamePlay ? (
+              ""
+            ) : (
+              <button className="newGame" onClick={startGame}>
+                Start new Game
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
